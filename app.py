5947f348407694138de284697b22c31895876acb
Thomas Flyttov Møller <thomas.flyttov@hotmail.com>
2025-04-01 01:22:16 +0000

    Update app.py to use uvicorn.run

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+import os
+import uvicorn
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Heimdal Webhook is live!"}
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
